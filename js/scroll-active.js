8803055d9cde69d9040a2de73db7c708f5c5d96c
zhe6289 <zhe1217_92@hotmail.com>
2019-05-19 17:29:14 +0800

    UX UI

--- js/scroll-active.js
+++ js/scroll-active.js
@@ -1,3 +1,4 @@
+//about
 $(document).ready(function () {
     var aboutEl = $('div.about');
     var aboutElOffset = aboutEl.offset().top / 2;
@@ -16,6 +17,64 @@ $(document).ready(function () {
     documentEl.on('scroll', function () {
       if (documentEl.scrollTop() > aboutElOffset && aboutEl.hasClass('hidden')){
           aboutEl.removeClass('hidden');
+      } 
+    });
+  })
+
+  //login about
+  $(document).ready(function () {
+    var aboutEl = $('.section-3 .login-about');
+    var aboutElOffset = aboutEl.offset().top /1.2;
+    var  documentEl = $(document);
+
+    documentEl.on('scroll', function () {
+      if (documentEl.scrollTop() > aboutElOffset && aboutEl.hasClass('hidden')){
+          aboutEl.removeClass('hidden');
+      } 
+    });
+  })
+
+  //login UI
+  $(document).ready(function () {
+    var aboutEl = $('.section-3 .container-login');
+    var aboutElOffset = aboutEl.offset().top /1.2;
+    var  documentEl = $(document);
+
+    documentEl.on('scroll', function () {
+      if (documentEl.scrollTop() > aboutElOffset ){
+        aboutEl.css({
+          "transform" : "translateX(0)",
+          "opacity" : "1"
+        })
+      } 
+    });
+  })
+
+  //register about
+  $(document).ready(function () {
+    var aboutEl = $('.section-4 .register-about');
+    var aboutElOffset = aboutEl.offset().top / 1.2;
+    var  documentEl = $(document);
+
+    documentEl.on('scroll', function () {
+      if (documentEl.scrollTop() > aboutElOffset && aboutEl.hasClass('hidden')){
+          aboutEl.removeClass('hidden');
+      } 
+    });
+  })
+
+  //register form
+  $(document).ready(function () {
+    var aboutEl = $('.section-4 .container-register');
+    var aboutElOffset = aboutEl.offset().top /1.2;
+    var  documentEl = $(document);
+
+    documentEl.on('scroll', function () {
+      if (documentEl.scrollTop() > aboutElOffset ){
+        aboutEl.css({
+          "transform" : "translateX(0)",
+          "opacity" : "1"
+        })
       } 
     });
   })
